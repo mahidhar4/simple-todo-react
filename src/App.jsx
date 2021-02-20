bd968ebdaffb6eb05c3a14459bd1a45688ef9cb9
mahidhar4 <mahidhar.kumar@gmail.com>
2021-02-20 17:15:32 +0530

    2 files modified, 4 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { render } from "react-dom";
 import {
   BrowserRouter as Router,
   Switch,
+  Redirect,
   Route,
   Link,
   useRouteMatch,
   useParams
 } from "react-router-dom";
 import "./style.css";
+import Login from "./components/login";
+import Hello from "./Hello";
 
 export default function App() {
   // fetch("/mock.json").then(result => {
@@ -16,14 +19,19 @@ export default function App() {
   //     alert(JSON.stringify(resulted));
   //   });
   // });
+  let isUserLoggedIn = false;
+  let loggedInfo = sessionStorage.getItem("userInfo");
+  if (loggedInfo) {
+    loggedInfo = JSON.parse(loggedInfo);
+    isUserLoggedIn = loggedInfo.Id !== undefined;
+  }
 
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/dahboard">
-            <Hello name={this.state.name} />
-            <p>Start editing to see some magic happen :)</p>
+            {isUserLoggedIn ? <Hello name={"hello"} /> : <Redirect to="/" />}
           </Route>
           <Route path="/">
             <Login />
